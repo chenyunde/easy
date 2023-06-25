8d080fd23df3d4c38d8f04ba885a0ef3b9f0379e
chenyunde <2503143637@qq.com>
2023-06-25 14:10:56 +0800

    备注修改（提交测试）

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 func main() {
 	r := gin.Default()
 
-	// 权限验证中间件
+	// 权限验证中间件，需要权限验证的接口分组
 	auth := r.Group("/auth")
 	auth.Use(middleware.AuthMiddleware())
 
-	// 不需要权限验证
+	// 不需要权限验证可以访问的接口分组
 	noAuth := r.Group("/noauth")
 	noAuth.POST("/login", login.Login)
 
